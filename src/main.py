e407a959ab63798bc5afb30a702f64b9ea6445c1
gavinelwell <55037780+gavinelwell@users.noreply.github.com>
2025-10-24 00:19:30 -0400

    3 Strikes Gamemode
    Separate file eventhandlers.py created to store all event checkers and handlers to avoid bloat in main.py. The goal next is to create a new file to store the game that can be accessed by both eventhandlers.py and main.py so that they don't have to communicate variables
    
    3 strikes gamemode: The player has a total of 3 missed shots before the game terminates. For each missed shot, the circle radius decreases, making shots more difficult to hit.

--- src/main.py
+++ src/main.py
@@ -4,38 +4,61 @@ import eventhandlers
 import math
 import random
 
+# Screen
 screen_res = (1280, 720)
-
-circle_pos = (screen_res[0]/2, screen_res[1]/2)
-circle_rad = 100
-
 border_thickness = 10
+
+# Variables
+circle_rad = 100
+circle_pos = (screen_res[0]/2, screen_res[1]/2) # Initial circle position, center of screen
+
+# Screen borders, boundaries for circles to be placed on screen
 border_x = (border_thickness + circle_rad, screen_res[0] - (border_thickness + circle_rad))
 border_y = (border_thickness + circle_rad, screen_res[1] - (border_thickness + circle_rad))
 
+# Initialize pygame
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode(screen_res)
 
-def check_circle_collision() -> bool:
-    mouse_pos = pygame.mouse.get_pos()
+# Ingame variables
+hits = 0
+misses = 0
+running = True
 
-    if math.sqrt(math.pow(mouse_pos[0] - circle_pos[0], 2) + math.pow(mouse_pos[1] - circle_pos[1], 2)) <= circle_rad:
-        return True
-    else:
-        return False
-
-while True:
+# Game loop
+while running:
+    
     events = pygame.event.get()
+
     for event in events:
+
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1: #left click
-                if check_circle_collision():
+                mouse_pos = pygame.mouse.get_pos()
+
+                # Check if the circle was hit or missed
+                # *If hit, place a new circle in a random location on the screen and add 1 to the hits counter
+                # *If missed, add 1 to the misses counter and reduce the circle radius. If the misses counter gets to 3 strikes, terminate the program
+                if eventhandlers.Check_circle_collision(mouse_pos, circle_pos[0], circle_pos[1], circle_rad):
                     circle_pos = (random.randint(border_x[0], border_x[1]), random.randint(border_y[0], border_y[1]))
+                    hits += 1
+                else:
+                    misses += 1
+                    circle_rad -= 25
+                    if misses == 3:
+                        running = False
 
     screen.fill('lightblue')
     pygame.draw.circle(screen, "red", circle_pos, circle_rad)
 
     pygame.display.update()
+
+# Display score
+print("hits: ", hits)
+print("misses :", misses)
+
+pygame.quit()
+sys.exit()
